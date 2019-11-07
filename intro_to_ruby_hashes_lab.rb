350ea9382b1f22ba7c57d9ad614c80cb8ae1ebf7
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-07 23:20:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
   # complexity.
 	{:railroads => {
 		:pieces => 4,
-		:rent_in_dollars => {:first => 25, :second => 50, :third => 100, :fourth => 200}
+		:rent_in_dollars => {:first => 25, :second => 50, :third => 100, :fourth => 200},
 		:names => {}
 	 }
 	}
